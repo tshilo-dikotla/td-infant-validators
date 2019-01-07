1dba87f70de78777cda372439dc3f031c706084b
Ame Diphoko <diphokoame@gmail.com>
2019-01-07 15:12:14 +0200

    adding infant birth arv form validations and tests

---
+++ td_infant_validators/form_validators/infant_birth_arv_form_validation.py
@@ -0,0 +1,30 @@
+from edc_constants.constants import YES
+from edc_form_validators import FormValidator
+
+
+class InfantArvFormValidator(FormValidator):
+
+    def clean(self):
+        self.required_if(
+            YES,
+            field='azt_after_birth',
+            field_required='azt_dose_date',
+            required_msg='Provide date of the first dose for AZT.',
+            not_required_msg=(
+                'Participant indicated that AZT was NOT provided. '
+                'You cannot provide date of first dose')
+        )
+        self.applicable_if(
+            YES,
+            field='azt_after_birth',
+            field_applicable='azt_additional_dose'
+        )
+        self.required_if(
+            YES,
+            field='sdnvp_after_birth',
+            field_required='nvp_dose_date',
+            required_msg=('If infant has received single dose NVP then provide '
+                          'NVP date.'),
+            not_required_msg=('Participant indicated that NVP was NOT provided. '
+                              'You cannot provide date of first dose.')
+        )
---
+++ td_infant_validators/tests/test_infant_birth_arv_form_validation.py
@@ -0,0 +1,132 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from edc_base.utils import get_utcnow
+from edc_constants.constants import YES, NO, NOT_APPLICABLE, UNKNOWN
+from ..form_validators import InfantArvFormValidator
+
+
+class TestInfantBirthArvFormValidator(TestCase):
+
+    def test_azt_after_birth_yes_azt_dose_date_required(self):
+        cleaned_data = {
+            'azt_after_birth': YES,
+            'azt_dose_date': None
+        }
+        form_validator = InfantArvFormValidator(cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('azt_dose_date', form_validator._errors)
+
+    def test_azt_after_birth_yes_azt_dose_date_provided(self):
+        cleaned_data = {
+            'azt_after_birth': YES,
+            'azt_dose_date': get_utcnow().date()
+        }
+        form_validator = InfantArvFormValidator(cleaned_data=cleaned_data)
+        try:
+            form_validator.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError raised unepectedly. Got{e}')
+
+    def test_azt_after_birth_no_azt_dose_date_invalid(self):
+        cleaned_data = {
+            'azt_after_birth': NO,
+            'azt_dose_date': get_utcnow().date()
+        }
+        form_validator = InfantArvFormValidator(cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('azt_dose_date', form_validator._errors)
+
+    def test_azt_after_birth_no_azt_dose_date_none_valid(self):
+        cleaned_data = {
+            'azt_after_birth': NO,
+            'azt_dose_date': None
+        }
+        form_validator = InfantArvFormValidator(cleaned_data=cleaned_data)
+        try:
+            form_validator.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError raised unepectedly. Got{e}')
+
+    def test_azt_after_birth_yes_azt_additional_dose_na_invalid(self):
+        cleaned_data = {
+            'azt_after_birth': YES,
+            'azt_dose_date': get_utcnow().date(),
+            'azt_additional_dose': NOT_APPLICABLE
+        }
+        form_validator = InfantArvFormValidator(cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('azt_additional_dose', form_validator._errors)
+
+    def test_azt_after_birth_yes_azt_additional_dose_valid(self):
+        cleaned_data = {
+            'azt_after_birth': YES,
+            'azt_dose_date': get_utcnow().date(),
+            'azt_additional_dose': UNKNOWN
+        }
+        form_validator = InfantArvFormValidator(cleaned_data=cleaned_data)
+        try:
+            form_validator.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError raised unepectedly. Got{e}')
+
+    def test_azt_after_birth_no_azt_additional_dose_invalid(self):
+        cleaned_data = {
+            'azt_after_birth': NO,
+            'azt_dose_date': None,
+            'azt_additional_dose': UNKNOWN
+        }
+        form_validator = InfantArvFormValidator(cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('azt_additional_dose', form_validator._errors)
+
+    def test_azt_after_birth_no_azt_additional_dose_na_valid(self):
+        cleaned_data = {
+            'azt_after_birth': NO,
+            'azt_dose_date': None,
+            'azt_additional_dose': NOT_APPLICABLE
+        }
+        form_validator = InfantArvFormValidator(cleaned_data=cleaned_data)
+        try:
+            form_validator.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError raised unepectedly. Got{e}')
+
+    def test_sdnvp_after_birth_yes_nvp_dose_date_required(self):
+        cleaned_data = {
+            'sdnvp_after_birth': YES,
+            'nvp_dose_date': None
+        }
+        form_validator = InfantArvFormValidator(cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('nvp_dose_date', form_validator._errors)
+
+    def test_sdnvp_after_birth_yes_nvp_dose_date_provided(self):
+        cleaned_data = {
+            'sdnvp_after_birth': YES,
+            'nvp_dose_date': get_utcnow().date()
+        }
+        form_validator = InfantArvFormValidator(cleaned_data=cleaned_data)
+        try:
+            form_validator.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError raised unepectedly. Got{e}')
+
+    def test_sdnvp_after_birth_no_nvp_dose_date_invalid(self):
+        cleaned_data = {
+            'sdnvp_after_birth': NO,
+            'nvp_dose_date': get_utcnow().date()
+        }
+        form_validator = InfantArvFormValidator(cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('nvp_dose_date', form_validator._errors)
+
+    def test_sdnvp_after_birth_no_nvp_dose_date_none_valid(self):
+        cleaned_data = {
+            'sdnvp_after_birth': NO,
+            'nvp_dose_date': None
+        }
+        form_validator = InfantArvFormValidator(cleaned_data=cleaned_data)
+        try:
+            form_validator.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError raised unepectedly. Got{e}')
