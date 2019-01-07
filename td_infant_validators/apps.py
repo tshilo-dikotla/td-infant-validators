212a69a0f2d4b6b05745cfbb606b9cb26f787e69
Ame Diphoko <diphokoame@gmail.com>
2019-01-07 11:11:10 +0200

    form validator and tests package

---
+++ td_infant_validators/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig as DjangoApponfig
+
+
+class AppConfig(DjangoApponfig):
+    name = 'td_infant_validators'
+    verbose_name = 'Tshilo Dikotla Maternal Form Validators'
