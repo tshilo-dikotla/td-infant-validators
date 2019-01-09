09336b48bfc417589bb5d9bfd4f63cfc30eaf49d
Ame Diphoko <diphokoame@gmail.com>
2019-01-09 07:34:58 +0200

    adding vaccines missed validations and tests

---
+++ td_infant_validators/form_validators/infant_vaccines_missed_form_validation.py
@@ -0,0 +1,28 @@
+from django.core.exceptions import ValidationError
+from edc_constants.constants import YES
+from edc_form_validators import FormValidator
+
+
+class VaccinesMissedFormValidator(FormValidator):
+
+    def clean(self):
+        self.validate_vaccine_missed(cleaned_data=self.cleaned_data)
+        self.validate_missed_vaccine_fields(cleaned_data=self.cleaned_data)
+
+    def validate_vaccine_missed(self, cleaned_data=None):
+        if cleaned_data.get('infant_fu_immunizations').vaccines_missed == YES:
+            if not cleaned_data.get('missed_vaccine_name'):
+                msg = {'missed_vaccine_name':
+                       'You mentioned that vaccines were missed. Please '
+                       'indicate which ones on the table.'}
+                self._errors.update(msg)
+                raise ValidationError(msg)
+
+    def validate_missed_vaccine_fields(self, cleaned_data=None):
+        self.required_if_not_none(
+            field='missed_vaccine_name',
+            field_required='reason_missed',
+            required_msg=('You said {} vaccine was missed. Give a reason'
+                          ' for missing this vaccine'.format(
+                              cleaned_data.get('missed_vaccine_name')))
+        )
---
+++ td_infant_validators/tests/test_infant_vaccines_missed_form_validation.py
@@ -0,0 +1,99 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase, tag
+from edc_constants.constants import YES, NO
+from edc_base.utils import get_utcnow
+from .models import Appointment, InfantVisit, InfantFuImmunizations
+from ..form_validators import VaccinesMissedFormValidator
+
+
+@tag('missed')
+class TestVaccinesMissedFormValidator(TestCase):
+
+    def setUp(self):
+        subject_identifier = '22334'
+        appointment = Appointment.objects.create(
+            subject_identifier=subject_identifier,
+            appt_datetime=get_utcnow(),
+            visit_code='1000')
+        infant_visit = InfantVisit.objects.create(
+            subject_identifier=appointment.subject_identifier,
+            appointment=appointment)
+        self.infant_fu_immunizations = InfantFuImmunizations.objects.create(
+            infant_visit=infant_visit,
+            vaccines_received=NO,
+            vaccines_missed=YES)
+
+    def test_infant_fu_immunization_vaccine_missed_yes_name_required(self):
+        cleaned_data = {
+            'infant_fu_immunizations': self.infant_fu_immunizations,
+            'missed_vaccine_name': None,
+        }
+        form_validator = VaccinesMissedFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('missed_vaccine_name', form_validator._errors)
+
+    def test_infant_fu_immunization_vaccine_missed_yes_name_given(self):
+        cleaned_data = {
+            'infant_fu_immunizations': self.infant_fu_immunizations,
+            'missed_vaccine_name': 'Polio',
+            'reason_missed': 'missed scheduled vaccination',
+        }
+        form_validator = VaccinesMissedFormValidator(
+            cleaned_data=cleaned_data)
+        try:
+            form_validator.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError raised unexpectedly. Got{e}')
+
+    def test_missed_vaccine_name_given_reason_required(self):
+        cleaned_data = {
+            'infant_fu_immunizations': self.infant_fu_immunizations,
+            'missed_vaccine_name': 'Polio',
+            'reason_missed': None
+        }
+        form_validator = VaccinesMissedFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('reason_missed', form_validator._errors)
+
+    def test_missed_vaccine_name_given_reason_provided(self):
+        cleaned_data = {
+            'infant_fu_immunizations': self.infant_fu_immunizations,
+            'missed_vaccine_name': 'Polio',
+            'reason_missed': 'missed scheduled vaccination',
+        }
+        form_validator = VaccinesMissedFormValidator(
+            cleaned_data=cleaned_data)
+        try:
+            form_validator.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError raised unexpectedly. Got{e}')
+
+    def test_missed_vaccine_name_none_reason_invalid(self):
+        self.infant_fu_immunizations.vaccines_missed = NO
+        self.infant_fu_immunizations.save()
+        cleaned_data = {
+            'infant_fu_immunizations': self.infant_fu_immunizations,
+            'missed_vaccine_name': None,
+            'reason_missed': 'missed scheduled vaccination'
+        }
+        form_validator = VaccinesMissedFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('reason_missed', form_validator._errors)
+
+    def test_missed_vaccine_name_none_reason_none_valid(self):
+        self.infant_fu_immunizations.vaccines_missed = NO
+        self.infant_fu_immunizations.save()
+        cleaned_data = {
+            'infant_fu_immunizations': self.infant_fu_immunizations,
+            'missed_vaccine_name': None,
+            'reason_missed': None,
+        }
+        form_validator = VaccinesMissedFormValidator(
+            cleaned_data=cleaned_data)
+        try:
+            form_validator.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError raised unexpectedly. Got{e}')
