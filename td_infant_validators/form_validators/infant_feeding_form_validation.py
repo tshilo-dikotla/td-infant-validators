29b7d28f25e0b1b8a857c643fe5b3515ce4652ed
moffatmore <mofenyimoffat@gmail.com>
2019-01-11 10:28:25 +0200

    adding tests for infant feeding form

--- td_infant_validators/form_validators/infant_feeding_form_validation.py
+++ td_infant_validators/form_validators/infant_feeding_form_validation.py
@@ -11,89 +11,39 @@ class InfantFeedingFormValidator(FormValidator):
 
     def clean(self):
         #         self.validate_formula_intro_occur_previous()
-        self.validate_formula_intro_date_not_future()
-        cleaned_data = self.cleaned_data
-        infant_feeding = self.infant_feeding_cls.objects.filter(infant_visit__subject_identifier=cleaned_data.get(
-            'infant_visit').appointment.subject_identifier,
-            formula_intro_date__isnull=False).exclude(infant_visit=cleaned_data.get(
-                'infant_visit')).last()
-        condition = self.cleaned_data.get(
-            'formula_intro_occur') == YES and not infant_feeding
-
-        self.required_if_true(
-            condition,
-            field_required='formula_intro_date',
-            required_msg='Question3: If received formula milk | foods | liquids since last'
-            ' attended visit. Please provide intro date')
+        #         self.validate_formula_intro_date_not_future()
+        self.validate_cows_milk()
+        self.validate_took_other_milk()
+        self.validate_breast_milk_weaning()
+        self.validate_breast_milk_completely_weaned()
+        #         self.validate_most_recent_bm_range()
 
         self.required_if(
             YES,
             field='took_formula',
             field_required='is_first_formula',
             required_msg='Question7: Infant took formula, is this the '
-            'first reporting of infant formula use? Please provide YES or NO')
+            'first reporting of infant formula use? Please provide YES or NO',
+            not_required_msg='Question7: You mentioned that infant did not take formula,'
+            ' PLEASE DO NOT PROVIDE FIRST FORMULA USE INFO')
 
         self.required_if(
             YES,
             field='is_first_formula',
             field_required='date_first_formula',
             required_msg='If this is a first reporting of infant formula'
-            ' please provide date and if date is estimated')
+            ' please provide date and if date is estimated',
+            not_required_msg='Question8: You mentioned that infant did not take formula,'
+            ' PLEASE DO NOT PROVIDE DATE OF FIRST FORMULA USE')
 
         self.required_if(
             YES,
             field='is_first_formula',
             field_required='est_date_first_formula',
             required_msg='If this is a first reporting of infant formula'
-            ' please provide date and if date is estimated')
-
-        self.not_required_if(
-            NO,
-            field='is_first_formula',
-            field_required='date_first_formula',
-            required_msg='Question8: You mentioned that is not the '
-            'first reporting of infant formula PLEASE DO NOT PROVIDE DATE'
-        )
-
-        self.not_required_if(
-            NO,
-            field='is_first_formula',
-            field_required='date_first_formula',
-            required_msg='Question8: You mentioned that is not the '
-            'first reporting of infant formula PLEASE DO NOT PROVIDE DATE'
-        )
-
-        self.not_required_if(
-            NO,
-            field='is_first_formula',
-            field_required='est_date_first_formula',
-            required_msg='Question9: You mentioned that is not the '
-            'first reporting of infant formula PLEASE DO NOT PROVIDE EST DATE'
-        )
-
-        self.required_if(
-            YES,
-            field='took_formula',
-            field_required='is_first_formula',
-            required_msg='Question7: You mentioned that infant did not take formula,'
-            ' PLEASE DO NOT PROVIDE FIRST FORMULA USE INFO'
-        )
-
-        self.required_if(
-            YES,
-            field='took_formula',
-            field_required='date_first_formula',
-            required_msg='Question8: You mentioned that infant did not take formula,'
-            ' PLEASE DO NOT PROVIDE DATE OF FIRST FORMULA USE'
-        )
-
-        self.required_if(
-            YES,
-            field='took_formula',
-            field_required='est_date_first_formula',
-            required_msg='Question9: You mentioned that infant did not take formula,'
-            ' PLEASE DO NOT PROVIDE ESTIMATED DATE OF FIRST FORMULA USE'
-        )
+            ' please provide date and if date is estimated',
+            not_required_msg='Question9: You mentioned that infant did not'
+            ' take formula, PLEASE DO NOT PROVIDE ESTIMATED DATE')
 
         self.required_if(
             YES,
@@ -102,6 +52,18 @@ class InfantFeedingFormValidator(FormValidator):
             required_msg='Question15: The infant took milk from another animal, '
             'please specify which?'
         )
+        cleaned_data = self.cleaned_data
+        infant_feeding = self.infant_feeding_cls.objects.filter(infant_visit__subject_identifier=cleaned_data.get(
+            'infant_visit').appointment.subject_identifier,
+            formula_intro_date__isnull=False).exclude(infant_visit=cleaned_data.get(
+                'infant_visit')).last()
+        condition = self.cleaned_data.get(
+            'formula_intro_occur') == YES and not infant_feeding
+        self.required_if_true(
+            condition,
+            field_required='formula_intro_date',
+            required_msg='Question3: If received formula milk | foods | liquids since last'
+            ' attended visit. Please provide intro date')
 
     def validate_cows_milk(self):
         cleaned_data = self.cleaned_data
@@ -113,6 +75,73 @@ class InfantFeedingFormValidator(FormValidator):
             if not cleaned_data.get('cow_milk_yes') == 'N/A':
                 raise forms.ValidationError(
                     'Question13: Infant did not take cows milk. Answer is NOT APPLICABLE')
+
+    def validate_took_other_milk(self):
+        cleaned_data = self.cleaned_data
+        if cleaned_data.get('other_milk') == YES:
+            if not cleaned_data.get('other_milk_animal'):
+                raise forms.ValidationError(
+                    'Question15: The infant took milk'
+                    ' from another animal, please specify which?')
+            if cleaned_data.get('milk_boiled') == NOT_APPLICABLE:
+                raise forms.ValidationError('Question16:The infant took milk from another animal, answer'
+                                            ' cannot be N/A')
+        else:
+            if cleaned_data.get('other_milk_animal'):
+                raise forms.ValidationError('Question15: The infant did not take milk from any other animal, please'
+                                            ' do not provide the name of the animal')
+
+            if cleaned_data.get('milk_boiled') != NOT_APPLICABLE:
+                raise forms.ValidationError('Question16: The infant did not take milk from any other animal, the'
+                                            ' answer for whether the milk was boiled should be N/A')
+
+    def validate_breast_milk_weaning(self):
+        cleaned_data = self.cleaned_data
+        if cleaned_data.get('ever_breastfeed') == YES:
+            if cleaned_data.get('complete_weaning') != NOT_APPLICABLE:
+                raise forms.ValidationError('Question24: The infant has been breastfed since the last visit, The'
+                                            ' answer should be N/A')
+        else:
+            if cleaned_data.get('complete_weaning') == NOT_APPLICABLE:
+                raise forms.ValidationError('Question24: The infant has not been breastfed since the last visit, '
+                                            'The answer should not be N/A')
+
+    def validate_breast_milk_completely_weaned(self):
+        cleaned_data = self.cleaned_data
+        if (cleaned_data.get('ever_breastfeed') == YES and
+                cleaned_data.get('weaned_completely') == NO and
+                not cleaned_data.get('most_recent_bm')):
+            raise forms.ValidationError({'most_recent_bm': 'The infant has not'
+                                         ' been weaned off of breast milk. This field'
+                                         ' is required.'})
+        elif (cleaned_data.get('ever_breastfeed') == NO and
+                cleaned_data.get('complete_weaning') == YES and
+                cleaned_data.get('most_recent_bm')):
+            raise forms.ValidationError({'most_recent_bm': 'The infant has been'
+                                         ' weaned off of breast milk before last '
+                                         'attended scheduled visit. This field'
+                                         ' is not required.'})
+
+    def validate_most_recent_bm_range(self):
+        cleaned_data = self.cleaned_data
+        prev_infant_feeding = self.infant_feeding_cls.objects.filter(infant_visit__subject_identifier=cleaned_data.get(
+            'infant_visit').appointment.registered_subject.subject_identifier,
+            most_recent_bm__isnull=False,
+            report_datetime__lt=cleaned_data.get('report_datetime')).exclude(infant_visit=cleaned_data.get(
+                'infant_visit')).last()
+
+        if(self.instance.previous_infant_instance and
+           (cleaned_data.get('ever_breastfeed') == YES and
+                cleaned_data.get('weaned_completely') == YES)):
+
+            if prev_infant_feeding:
+                if(not cleaned_data.get('most_recent_bm')
+                   or (cleaned_data.get('most_recent_bm') > cleaned_data.get("report_datetime").date()
+                       or cleaned_data.get('most_recent_bm') < prev_infant_feeding.most_recent_bm)):
+
+                    raise forms.ValidationError({'most_recent_bm': 'Date of most '
+                                                 'recent breastfeeding must be '
+                                                 'between last visit date and today.'})
 
     @property
     def infant_feeding_cls(self):
--- td_infant_validators/tests/test_infant_feeding_form_validation.py
+++ td_infant_validators/tests/test_infant_feeding_form_validation.py
@@ -75,3 +75,159 @@ class TestInfantFeedingFormValidator(TestCase):
         forms.infantfeeding = 'td_infant_validators.infantfeeding'
         forms.infant_visit = 'td_infant_validators.infantfeeding'
         self.assertRaises(ValidationError, forms.validate)
+
+    def test_infant_formula_feeding_YES(self):
+        """"Test if the child took formula, the field for whether this is the first reporting in not N/A"""
+        self.options['is_first_formula'] = None
+        self.options['took_formula'] = YES
+        forms = InfantFeedingFormValidator(cleaned_data=self.options)
+        forms.infantfeeding = 'td_infant_validators.infantfeeding'
+        forms.infant_visit = 'td_infant_validators.infantfeeding'
+        self.assertRaises(ValidationError, forms.validate)
+
+    def test_infant_formula_feeding_not_yes(self):
+        """Test if the child did not take formula, the field for whether this is the first reporting is N/A not YES"""
+        self.options['took_formula'] = NO
+        forms = InfantFeedingFormValidator(cleaned_data=self.options)
+        forms.infantfeeding = 'td_infant_validators.infantfeeding'
+        forms.infant_visit = 'td_infant_validators.infantfeeding'
+        self.assertRaises(ValidationError, forms.validate)
+
+    def test_infant_formula_feeding_not_yes_date_provided(self):
+        """Test if the child did not take formula, the field for whether this is the first reporting is N/A not None"""
+        self.options['took_formula'] = NO
+        self.options['is_first_formula'] = None
+        forms = InfantFeedingFormValidator(cleaned_data=self.options)
+        forms.infantfeeding = 'td_infant_validators.infantfeeding'
+        forms.infant_visit = 'td_infant_validators.infantfeeding'
+        self.assertRaises(ValidationError, forms.validate)
+
+    def test_infant_formula_feeding_not_yes_est_date_provided(self):
+        """Test if the child did not take formula, the date of estimated first formula use is not provided"""
+        self.options['took_formula'] = NO
+        self.options['is_first_formula'] = None
+        self.options['date_first_formula'] = None
+        forms = InfantFeedingFormValidator(cleaned_data=self.options)
+        forms.infantfeeding = 'td_infant_validators.infantfeeding'
+        forms.infant_visit = 'td_infant_validators.infantfeeding'
+        self.assertRaises(ValidationError, forms.validate)
+
+    def test_is_first_formula_yes_no_date(self):
+        """Test that if this is the first reporting of infant formula,'
+        ' the date should be provided"""
+        self.options['date_first_formula'] = None
+        forms = InfantFeedingFormValidator(cleaned_data=self.options)
+        forms.infantfeeding = 'td_infant_validators.infantfeeding'
+        forms.infant_visit = 'td_infant_validators.infantfeeding'
+        self.assertRaises(ValidationError, forms.validate)
+
+    def test_is_first_formula_yes_date_estimated_field_none(self):
+        """Test that if this is the first reporting of infant formula, whether the date is estimated should be indicated"""
+        self.options['date_first_formula'] = NO
+        self.options['est_date_first_formula'] = None
+        forms = InfantFeedingFormValidator(cleaned_data=self.options)
+        forms.infantfeeding = 'td_infant_validators.infantfeeding'
+        forms.infant_visit = 'td_infant_validators.infantfeeding'
+        self.assertRaises(ValidationError, forms.validate)
+
+    def test_is_first_formula_no_date_provided(self):
+        """Test that if this is not the first reporting of infant formula, the date should not be provided"""
+        self.options['is_first_formula'] = NO
+        forms = InfantFeedingFormValidator(cleaned_data=self.options)
+        forms.infantfeeding = 'td_infant_validators.infantfeeding'
+        forms.infant_visit = 'td_infant_validators.infantfeeding'
+        self.assertRaises(ValidationError, forms.validate)
+
+    def test_is_first_formula_no_date_estimated_given(self):
+        """Test that if this is not the first reporting of infant formula, whether the date is estimated should not
+           be indicated"""
+        self.options['is_first_formula'] = NO
+        self.options['date_first_formula'] = None
+        forms = InfantFeedingFormValidator(cleaned_data=self.options)
+        forms.infantfeeding = 'td_infant_validators.infantfeeding'
+        forms.infant_visit = 'td_infant_validators.infantfeeding'
+        self.assertRaises(ValidationError, forms.validate)
+
+    def test_took_cow_milk_yes(self):
+        """test that if the infant received cow milk, the field question13 should not be N/A"""
+        self.options['cow_milk_yes'] = NOT_APPLICABLE
+        forms = InfantFeedingFormValidator(cleaned_data=self.options)
+        forms.infantfeeding = 'td_infant_validators.infantfeeding'
+        forms.infant_visit = 'td_infant_validators.infantfeeding'
+        self.assertRaises(ValidationError, forms.validate)
+
+    def test_took_cow_milk__not_yes(self):
+        """test that if the infant did not receive cow milk, the field question13 should be N/A"""
+        self.options['cow_milk'] = NO
+        forms = InfantFeedingFormValidator(cleaned_data=self.options)
+        forms.infantfeeding = 'td_infant_validators.infantfeeding'
+        forms.infant_visit = 'td_infant_validators.infantfeeding'
+        self.assertRaises(ValidationError, forms.validate)
+
+    def test_took_milk_other_yes_animal_not_specified(self):
+        """Test that if the infant took milk from another animal, that animal is specified"""
+        self.options['other_milk'] = YES
+        forms = InfantFeedingFormValidator(cleaned_data=self.options)
+        forms.infantfeeding = 'td_infant_validators.infantfeeding'
+        forms.infant_visit = 'td_infant_validators.infantfeeding'
+        self.assertRaises(ValidationError, forms.validate)
+
+    def test_took_milk__yes_milk_boiled_not_applicable(self):
+        """Test that if the infant took milk from another animal, the answer to Question16 is not N/A"""
+        self.options['other_milk'] = YES
+        self.options['other_milk_animal'] = 'Goat'
+        self.options['milk_boiled'] = NOT_APPLICABLE
+        forms = InfantFeedingFormValidator(cleaned_data=self.options)
+        forms.infantfeeding = 'td_infant_validators.infantfeeding'
+        forms.infant_visit = 'td_infant_validators.infantfeeding'
+        self.assertRaises(ValidationError, forms.validate)
+
+    def test_took_milk_other_not_yes_animal_specified(self):
+        """Test that if the infant did not take milk from another animal, that animal is not specified"""
+        self.options['other_milk_animal'] = 'Goat'
+        forms = InfantFeedingFormValidator(cleaned_data=self.options)
+        forms.infantfeeding = 'td_infant_validators.infantfeeding'
+        forms.infant_visit = 'td_infant_validators.infantfeeding'
+        self.assertRaises(ValidationError, forms.validate)
+
+    def test_took_milk_other_not_yes_boiled_not_not_applicable(self):
+        """Test that if the infant did not take milk from another animal, the answer to question 16 is N/A"""
+        self.options['milk_boiled'] = YES
+        forms = InfantFeedingFormValidator(cleaned_data=self.options)
+        forms.infantfeeding = 'td_infant_validators.infantfeeding'
+        forms.infant_visit = 'td_infant_validators.infantfeeding'
+        self.assertRaises(ValidationError, forms.validate)
+
+    def test_child_breastfed_complete_weaning_not_not_applicable(self):
+        """Test that if the infant has been breast fed since the last visit, the answer to question24 is N/A"""
+        self.options['complete_weaning'] = NO
+        forms = InfantFeedingFormValidator(cleaned_data=self.options)
+        forms.infantfeeding = 'td_infant_validators.infantfeeding'
+        forms.infant_visit = 'td_infant_validators.infantfeeding'
+        self.assertRaises(ValidationError, forms.validate)
+
+    def test_child_not_breastfed_complete_weaning_not_applicable(self):
+        """Test that if the child has not been breast fed since the last visit, the answer to question24 should not be
+        NA"""
+        self.options['ever_breastfeed'] = NO
+        self.options['complete_weaning'] = NOT_APPLICABLE
+        forms = InfantFeedingFormValidator(cleaned_data=self.options)
+        forms.infantfeeding = 'td_infant_validators.infantfeeding'
+        forms.infant_visit = 'td_infant_validators.infantfeeding'
+        self.assertRaises(ValidationError, forms.validate)
+
+    @tag('2')
+    def test_formula_intro_occur_yes_no_foods_indicated(self):
+        self.options['juice'] = NO
+        self.options['cow_milk'] = NO
+        self.options['cow_milk_yes'] = NOT_APPLICABLE
+        self.options['other_milk'] = NO
+        self.options['fruits_veg'] = NO
+        self.options['cereal_porridge'] = NO
+        self.options['solid_liquid'] = NO
+        forms = InfantFeedingFormValidator(cleaned_data=self.options)
+        forms.infantfeeding = 'td_infant_validators.infantfeeding'
+        forms.infant_visit = 'td_infant_validators.infantfeeding'
+        self.assertRaises(ValidationError, forms.validate)
+#         self.assertIn("You should answer YES on either one of the questions about the juice, cow_milk, other milk, "
+#                       "fruits_veg, cereal_porridge or solid_liquid", forms.errors.get('__all__'))
