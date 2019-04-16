cd260cadeebf4ecfc4eb14aeadc3228e469874f4
tumiemosweu <imosweuk@gmail.com>
2019-04-16 15:38:09 +0200

    fix visit validation

--- td_infant_validators/form_validators/infant_visit_form_validation.py
+++ td_infant_validators/form_validators/infant_visit_form_validation.py
@@ -6,7 +6,7 @@ class InfantVisitFormValidator(InfantFormValidatorMixin, FormValidator):
 
     def clean(self):
         cleaned_data = self.cleaned_data
-        infant_identifier = cleaned_data.get('subject_identifier')
+        infant_identifier = cleaned_data.get('appointment').subject_identifier
         report_datetime = cleaned_data.get('report_datetime')
         self.validate_against_birth_date(infant_identifier=infant_identifier,
                                          report_datetime=report_datetime)
