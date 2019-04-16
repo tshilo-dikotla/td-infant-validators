752ca7540e13e19b2cc52a79e10fa97fc1155448
tumiemosweu <imosweuk@gmail.com>
2019-04-16 11:26:12 +0200

    fix infant fu form

--- td_infant_validators/form_validators/infant_fu_form_validation.py
+++ td_infant_validators/form_validators/infant_fu_form_validation.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ValidationError
 from edc_constants.constants import YES
 from edc_form_validators import FormValidator
 
@@ -12,14 +11,3 @@ class InfantFuFormValidator(FormValidator):
             field_required='days_hospitalized',
             required_msg='If infant was hospitalized, '
             'please provide # of days hospitalized')
-        try:
-            if self.cleaned_data.get('days_hospitalized') is not None:
-                condition = self.cleaned_data.get('days_hospitalized') <= 90
-                self.applicable_if_true(
-                    condition,
-                    field_applicable='days_hospitalized',
-                    not_applicable_msg='days hospitalized cannot be greater than 90days'
-                )
-        except ValidationError:
-            self._errors.update('Total number of days hospitalized required')
-            raise ValidationError('Total number of days hospitalized required')
