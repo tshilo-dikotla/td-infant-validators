ba23f8f4ba6203524f577c35c014ade851326b0e
tumiemosweu <imosweuk@gmail.com>
2019-04-16 13:57:49 +0200

    clean up infant

--- td_infant_validators/form_validators/infant_birth_data_form_validation.py
+++ td_infant_validators/form_validators/infant_birth_data_form_validation.py
@@ -1,50 +1,26 @@
-from django.core.exceptions import ValidationError
-from edc_constants.constants import (
-    NO, NOT_APPLICABLE, UNKNOWN, YES)
+from edc_constants.constants import YES
 from edc_form_validators import FormValidator
 
+from .form_validator_mixin import InfantFormValidatorMixin
 
-class InfantBirthDataFormValidator(FormValidator):
+
+class InfantBirthDataFormValidator(InfantFormValidatorMixin, FormValidator):
 
     def clean(self):
         cleaned_data = self.cleaned_data
-        self.validate_apgar_score(cleaned_data)
+        infant_identifier = cleaned_data.get('infant_visit').subject_identifier
+        self.validate_against_birth_date(infant_identifier=infant_identifier,
+                                         report_datetime=cleaned_data.get(
+                                             'report_datetime'))
+        self.validate_apgar_score()
 
-    def validate_apgar_score(self, cleaned_data):
-        if cleaned_data.get('apgar_score') == YES:
-            if not cleaned_data.get('apgar_score_min_1') == 0:
-                if not cleaned_data.get('apgar_score_min_1'):
-                    self._errors.update(
-                        {'apgar_score_min_1':
-                         'If Apgar scored performed, then you should answer At 1 minute(Q7).'
-                         })
-                    raise ValidationError(
-                        'If Apgar scored performed, then you should answer At 1 minute(Q7).')
-            if not cleaned_data.get('apgar_score_min_5') == 0:
-                if not cleaned_data.get('apgar_score_min_5'):
-                    self._errors.update(
-                        {'apgar_score_min_5':
-                         'If Apgar scored performed, then you should answer At 5 minute(Q8).'
-                         })
-                    raise ValidationError(
-                        'If Apgar scored performed, then you should answer At 5 minute(Q8).')
-        else:
-            if cleaned_data.get('apgar_score_min_1'):
-                self._errors.update(
-                    {'apgar_score_min_1':
-                        'If Apgar scored was NOT performed, then you should NOT answer at '
-                     '1 minute(Q7).'})
-                raise ValidationError('If Apgar scored was NOT performed, then you should NOT answer at '
-                                      '1 minute(Q7).')
-            if cleaned_data.get('apgar_score_min_5'):
+    def validate_apgar_score(self):
+        agpar_list = ['apgar_score_min_1', 'apgar_score_min_5',
+                      'apgar_score_min_10']
 
-                self._errors.update(
-                    {'apgar_score_min_5': 'If Apgar scored was NOT performed, then you should NOT answer at 5 '
-                     'minute(Q8).'})
-                raise ValidationError('If Apgar scored was NOT performed, then you should NOT answer at 5 '
-                                      'minute(Q8).')
-            if cleaned_data.get('apgar_score_min_10'):
-                self._errors.update({'apgar_score_min_10': 'If Apgar scored was NOT performed, then you should NOT answer at 10 '
-                                     'minute(Q9).'})
-                raise ValidationError('If Apgar scored was NOT performed, then you should NOT answer at 10 '
-                                      'minute(Q9).')
+        for agpar in agpar_list:
+            self.required_if(
+                YES,
+                field='apgar_score',
+                field_required=agpar,
+                required_msg='If Apgar scored performed, this field is required.')
