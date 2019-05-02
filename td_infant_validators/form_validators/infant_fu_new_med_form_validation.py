cc1b7bf47ce4ab91f13d825fb0bead3114ea6700
tumiemosweu <imosweuk@gmail.com>
2019-05-02 16:26:47 +0200

    update fu new med validations

--- td_infant_validators/form_validators/infant_fu_new_med_form_validation.py
+++ td_infant_validators/form_validators/infant_fu_new_med_form_validation.py
@@ -5,6 +5,13 @@ from edc_form_validators import FormValidator
 from .form_validator_mixin import InfantFormValidatorMixin
 
 
+class InfantFuNewMedFormValidator(InfantFormValidatorMixin, FormValidator):
+
+    def clean(self):
+        self.validate_against_visit_datetime(
+            self.cleaned_data.get('report_datetime'))
+
+
 class InfantFuNewMedItemsFormValidator(InfantFormValidatorMixin, FormValidator):
 
     def clean(self):
