880371190f3452159e305bc7c5c13b3c4a4e0c43
tumiemosweu <imosweuk@gmail.com>
2019-05-14 10:40:20 +0200

    fix infant requisition validation

--- td_infant_validators/form_validators/infant_requisition_form_validator.py
+++ td_infant_validators/form_validators/infant_requisition_form_validator.py
@@ -9,10 +9,11 @@ class InfantRequisitionFormValidator(InfantFormValidatorMixin,
                                      FormValidator):
 
     def clean(self):
-        if (self.cleaned_data.get('panel').name == 'dna_pcr'
-                and self.cleaned_data.get('item_type') == TUBE):
-            msg = {'item_type':
-                   'DNA PCR panel must have DBS Card collection type.'
-                   'Please correct.'}
-            self._errors.update(msg)
-            raise ValidationError(msg)
+        if self.cleaned_data.get('panel'):
+            if (self.cleaned_data.get('panel').name == 'dna_pcr'
+                    and self.cleaned_data.get('item_type') == TUBE):
+                msg = {'item_type':
+                       'DNA PCR panel must have DBS Card collection type.'
+                       'Please correct.'}
+                self._errors.update(msg)
+                raise ValidationError(msg)
