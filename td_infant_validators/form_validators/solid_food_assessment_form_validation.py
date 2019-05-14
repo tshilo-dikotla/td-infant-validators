ad5f93c5156b12a17519d5a6e7d8d4adecd52be7
tumiemosweu <imosweuk@gmail.com>
2019-05-14 11:31:46 +0200

    fix solid food assesment validations

--- td_infant_validators/form_validators/solid_food_assessment_form_validation.py
+++ td_infant_validators/form_validators/solid_food_assessment_form_validation.py
@@ -11,109 +11,29 @@ class SolidFoodAssessementFormValidator(InfantFormValidatorMixin, FormValidator)
         self.validate_against_visit_datetime(
             self.cleaned_data.get('report_datetime'))
 
-        self.required_if(
-            YES,
-            field='porridge',
-            field_required='porridge_freq',
-            required_msg='Question6: Please indicate how many times this child has had porridge in the last week'
-        )
-        self.required_if(
-            YES,
-            field='tsabana',
-            field_required='tsabana_week',
-            required_msg='Question8: Please indicate how many times this child has had tsabana in the last week'
-        )
+        fields = ['porridge', 'tsabana', 'meat', 'potatoes', 'carrot_swt_potato',
+                  'green_veg', 'fresh_fruits', 'fullcream_milk', 'skim_milk',
+                  'raw_milk', 'juice', 'eggs', 'yogurt', 'cheese']
 
-        self.required_if(
-            YES,
-            field='meat',
-            field_required='meat_freq',
-            required_msg='Question11: Please indicate how many times the child has had any meat, chicken or fish'
-        )
+        special_fields = {'meat': 'meat, chicken or fish',
+                          'carrot_swt_potato': 'carrot, pumpkin or sweet potato',
+                          'green_veg': 'green vegetables'}
 
-        self.required_if(
-            YES,
-            field='potatoes',
-            field_required='potatoes_freq',
-            required_msg='Question13: Please indicate how many times the child has had any potatoes'
-        )
+        for field in fields:
+            if field == 'tsabana':
+                f_required = 'tsabana_week'
+            else:
+                f_required = field + '_freq'
 
-        self.required_if(
-            YES,
-            field='carrot_swt_potato',
-            field_required='carrot_swt_potato_freq',
-            required_msg='Question15: Please indicate how many times this child has had carrot, pumpkin or sweet potato'
-        )
+            f_message = special_fields.get('field') or field.split('_')
 
-        self.required_if(
-            YES,
-            field='carrot_swt_potato',
-            field_required='carrot_swt_potato_freq',
-            required_msg='Question15: Please indicate how many times this child has had carrot, pumpkin or sweet potato'
-        )
-
-        self.required_if(
-            YES,
-            field='green_veg',
-            field_required='green_veg_freq',
-            required_msg='Question17: Please indicate how many times this child has had green vegetables in the last week'
-        )
-
-        self.required_if(
-            YES,
-            field='fresh_fruits',
-            field_required='fresh_fruits_freq',
-            required_msg='Question19: Please indicate how many times this child has had fresh fruits in the last week'
-        )
-
-        self.required_if(
-            YES,
-            field='fullcream_milk',
-            field_required='fullcream_milk_freq',
-            required_msg='Question21: Please indicate how many times this child has had full cream milk in the last week'
-        )
-
-        self.required_if(
-            YES,
-            field='skim_milk',
-            field_required='skim_milk_freq',
-            required_msg='Question23: Please indicate how many times this child has had skim milk in the last week'
-        )
-
-        self.required_if(
-            YES,
-            field='raw_milk',
-            field_required='raw_milk_freq',
-            required_msg='Question25: Please indicate how many times this child has had raw milk in the last week'
-        )
-
-        self.required_if(
-            YES,
-            field='juice',
-            field_required='juice_freq',
-            required_msg='Question27: Please indicate how many times this child has had juice in the last week'
-        )
-
-        self.required_if(
-            YES,
-            field='eggs',
-            field_required='eggs_freq',
-            required_msg='Question29: Please indicate how many times this child has had eggs in the last week'
-        )
-
-        self.required_if(
-            YES,
-            field='yogurt',
-            field_required='yogurt_freq',
-            required_msg='Question31: Please indicate how many times this child has had yogurt in the last week'
-        )
-
-        self.required_if(
-            YES,
-            field='cheese',
-            field_required='cheese_freq',
-            required_msg='Question33: Please indicate how many times this child has had Cheese in the last week'
-        )
+            self.required_if(
+                YES,
+                field=field,
+                field_required=f_required,
+                required_msg=('Please indicate how many times this '
+                              f'child has had {f_message} in the last week.')
+            )
 
         self.m2m_other_specify(
             'Other',
